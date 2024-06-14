b8108a698e9cbcb10b140355c9ef5ea68c0a3997
Sandeep Kumar Tak <stak@c2r3p1.hive.fi>
2024-06-14 15:36:44 +0300

    addded

--- fractol.h
+++ fractol.h
@@ -6,16 +6,38 @@
 /*   By: stak <stak@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 11:39:24 by stak              #+#    #+#             */
-/*   Updated: 2024/06/12 11:45:59 by stak             ###   ########.fr       */
+/*   Updated: 2024/06/14 14:57:08 by stak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
 # define FRACTOL_H
 
-# include "libft.h"
+# include "libft/libft.h"
+# include "ft_printf.h"
+# include "mlx/mlx.h"
 # include <math.h>
 # include <pthread.h>
-# include <mlx.h>
+
+
+typedef struct s_fractal
+{
+	char			*name;
+	mlx_t			*mlx_connection;
+	void			*mlx_new_window;
+	mlx_image_t		*img;
+	void			*colors;
+	double			esc_value;
+	int				iteration_def;
+	double			shift_x;
+	double			shift_y;
+	double			julia_x;
+	double			julia_y;
+	double			zoom;
+	double			x;
+	double			y;
+}	t_fractal;
+
+
 
 #endif
